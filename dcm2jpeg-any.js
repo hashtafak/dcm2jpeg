f735175f3b2b46cf5a1525ab0ffb1328d59f4cde
Tran T. Anh <jamestran201@gmail.com>
2020-04-01 18:09:35 +0700

    update cs solution

---
+++ dcm2jpeg-any.js
@@ -0,0 +1,24 @@
+const start = new Date()
+const hrstart = process.hrtime()
+
+const edge = require('edge-js');
+const dotNetFunction = edge.func({
+    assemblyFile: '.\\Debug\\dcm2jpeg.dll',
+    typeName: 'Startup',
+    methodName: 'Invoke'
+});
+const path = "demo.dcm"
+
+dotNetFunction(path, function (error, result) {
+    console.log(`error: ${error}`)
+    
+    const end = new Date() - start,
+        hrend = process.hrtime(hrstart)
+
+    console.info('Execution time: %dms', end)
+    console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
+
+    const buffer = Buffer.from(result);
+    console.log("Byte length: " + buffer.length);
+    console.log("KB: " + buffer.length / 1e+3);
+});
