12e6e9f2953dcd41e7d8fdd2a250aeb534c7d705
Tran T. Anh <jamestran201@gmail.com>
2020-04-01 17:51:18 +0700

    init

---
+++ DicomToJpegMemoryStream.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Dicom.Imaging;
+
+namespace DicomToJpegDLL
+{
+    class DicomToJpegMemoryStream
+    {
+        public MemoryStream Convert(string filePath)
+        {
+            var image = new DicomImage(filePath);
+            Bitmap btm = image.RenderImage().AsClonedBitmap();
+
+            MemoryStream ms = new MemoryStream();
+            btm.Save(ms, ImageFormat.Jpeg);
+
+            //Console.WriteLine(btm.Width);
+            //Console.WriteLine(btm.Height);
+            return ms;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using DicomToJpegDLL;
+
+public class Startup
+{
+    #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+    public async Task<object> Invoke(object input)
+    #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+    {
+        string filePath = (string)input;
+        DicomToJpegMemoryStream dcm2jpeg = new DicomToJpegMemoryStream();
+        MemoryStream ms = dcm2jpeg.Convert(filePath);
+        byte[] byteImage = ms.ToArray();
+
+        // Get Base64
+        var SigBase64 = Convert.ToBase64String(byteImage);
+        return SigBase64;
+    }
+}
